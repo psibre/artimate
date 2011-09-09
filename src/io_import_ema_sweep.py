eb4db0a75e05f9909d1feaa575322b1b53d1f559
Ingmar Steiner (burth.loria.fr) <ingmar.steiner@inria.fr>
2011-09-09 17:01:56 +0200

    adapted for use with PyDev
    using MacPython 3.2.2 with Blender Game Engine API pypredefs generated using
    http://phatcore.com/projects/files/blender-bge-api.zip

--- src/io_import_ema_sweep.py
+++ src/io_import_ema_sweep.py
@@ -4,8 +4,8 @@
 bl_info = {
     "name": "AG500 EMA pos file importer (.pos)",
     "author": "Ingmar Steiner",
-    "version": (0,0,1),
-    "blender": (2,5,8),
+    "version": (0, 0, 1),
+    "blender": (2, 5, 8),
     "location": "File > Import > Import AG500 EMA sweep (.pos file)",
     "description": "Import AG500 EMA sweep from .pos file",
     "warning": "",
@@ -13,11 +13,18 @@ bl_info = {
     "category": "Import-Export"}
 
 import bpy, os, math
+import bpy_extras
 from array import array
-from bpy.props import *
-from add_utils import *
-from bpy_extras.io_utils import ImportHelper
 
+# namespace laziness
+context = bpy.context
+data = bpy.data
+io_utils = bpy_extras.io_utils
+ops = bpy.ops
+props = bpy.props
+types = bpy.types
+utils = bpy.utils
+    
 ##### CLASSES #####
 
 class Sweep:
@@ -40,9 +47,9 @@ class Sweep:
         return data
 
     def decimate(self):
-        for channel in data.keys():
-            decimated = decimate(data[channel])
-            data[channel] = decimated
+        for channel in self.data.keys():
+            decimated = decimate(self.data[channel])
+            self.data[channel] = decimated
 
     def coils(self):
         coils = [channel.split('_')[0]
@@ -50,20 +57,20 @@ class Sweep:
         return coils
     coils = property(coils)
 
-    def getLoc(self, coil, frame = 0):
+    def getLoc(self, coil, frame=0):
         x = self.data[coil + "_X"][frame]
         y = self.data[coil + "_Y"][frame]
         z = self.data[coil + "_Z"][frame]
         return x, y, z
 
-    def getRot(self, coil, frame = 0):
+    def getRot(self, coil, frame=0):
         phi_deg = self.data[coil + "_phi"][frame]
         theta_deg = self.data[coil + "_theta"][frame]
         phi_rad = math.radians(phi_deg)
         theta_rad = math.radians(theta_deg)
         return phi_rad, theta_rad, 0
 
-    def getValue(self, coil, index, frame = 0):
+    def getValue(self, coil, index, frame=0):
         values = self.getLoc(coil, frame) + self.getRot(coil, frame)
         return values[index]
 
@@ -76,51 +83,51 @@ def generate_paths(self):
     else:
         return directory
 
-def generate_header(self, num_coils = 12):
+def generate_header(self, num_coils=12):
     # hard-coded for AG500
     dimensions = ["X", "Y", "Z", "phi", "theta", "RMS", "Extra"]
     channels = []
     for coil in range(num_coils):
-        channels.extend(["Channel%02d_%s" % (coil+1, dimension)
+        channels.extend(["Channel%02d_%s" % (coil + 1, dimension)
                          for dimension in dimensions])
     return channels
 
 def generate_coil_objects(sweep):
     # add empty EMA node as parent of all coils:
-    bpy.ops.object.add()
-    bpy.context.active_object.name = "EMA"
+    ops.object.add()
+    context.active_object.name = "EMA"
 
     # make coil objects:
     for coil in sweep.coils:
         # remember, the size parameters are scaled down:
         depth = 10.0
-        bpy.ops.mesh.primitive_cone_add(vertices = 8,
-                                        radius = 2.5,
-                                        depth = depth,
-                                        location = (0, 0, depth / 2))
-        bpy.context.active_object.name = coil + "Coil"
+        ops.mesh.primitive_cone_add(vertices=8,
+                                        radius=2.5,
+                                        depth=depth,
+                                        location=(0, 0, depth / 2))
+        context.active_object.name = coil + "Coil"
 
         # move object origin so that it lies at center of cone base:
-        bpy.context.scene.cursor_location = 0, 0, 0
-        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+        context.scene.cursor_location = 0, 0, 0
+        ops.object.origin_set(type='ORIGIN_CURSOR')
 
         # THEN locate and rotate:
-        bpy.context.active_object.location = sweep.getLoc(coil)
-        bpy.context.active_object.rotation_euler = sweep.getRot(coil)
+        context.active_object.location = sweep.getLoc(coil)
+        context.active_object.rotation_euler = sweep.getRot(coil)
 
         # make child of EMA node:
-        bpy.ops.object.select_name(name = "EMA", extend = True)
-        bpy.ops.object.parent_set()
+        ops.object.select_name(name="EMA", extend=True)
+        ops.object.parent_set()
 
-def generate_animation(sweep, start_frame = 1, end_frame = -1):
+def generate_animation(sweep, start_frame=1, end_frame= -1):
     end_frame = sweep.size if end_frame == -1 else end_frame
     assert end_frame - start_frame <= sweep.size
 
     for coil_name in sweep.coils:
-        coil = bpy.data.objects[coil_name + "Coil"]
+        coil = data.objects[coil_name + "Coil"]
 
         coil.animation_data_create()
-        coil.animation_data.action = bpy.data.actions.new(coil_name+"Action")
+        coil.animation_data.action = data.actions.new(coil_name + "Action")
         fcurves = coil.animation_data.action.fcurves
 
         fcurves.new(data_path="location", index=0)
@@ -160,7 +167,7 @@ def import_sweep(self, context):
     generate_coil_objects(sweep)
     generate_animation(sweep, self.start_frame, self.end_frame)
     # hack: downscale
-    bpy.data.objects["EMA"].scale = (0.1, 0.1, 0.1)
+    data.objects["EMA"].scale = (0.1, 0.1, 0.1)
 
     # TODO: use API to set FPS to 25, Framerate Base to 0.125
     # for 200 Hz (adjust dependng on self.sampling_freq)
@@ -172,7 +179,7 @@ def import_sweep(self, context):
 
 ##### OPERATOR #####
 
-class IMPORT_OT_image_to_plane(bpy.types.Operator, ImportHelper, AddObjectHelper):
+class IMPORT_OT_image_to_plane(types.Operator, io_utils.ImportHelper):
     ''''''
     bl_idname = "import.ema_sweep"
     bl_label = "Import EMA Sweep from .pos file"
@@ -180,17 +187,17 @@ class IMPORT_OT_image_to_plane(bpy.types.Operator, ImportHelper, AddObjectHelper
     bl_options = {'REGISTER', 'UNDO'}
 
     ## OPTIONS ##
-    sampling_freq = IntProperty(name="Sampling frequency",
+    sampling_freq = props.IntProperty(name="Sampling frequency",
                                 description="Number of frames per second",
                                 min=1,
                                 default=200)
-    start_frame = IntProperty(name="Start frame",
+    start_frame = props.IntProperty(name="Start frame",
                               description="Number of first frame to import",
                               default=1)
-    end_frame = IntProperty(name="End frame",
+    end_frame = props.IntProperty(name="End frame",
                               description="Number of last frame to import (-1 = last)",
-                              default=-1)
-    header_file_name = StringProperty(name="Header file name",
+                              default= -1)
+    header_file_name = props.StringProperty(name="Header file name",
                                       description="List of EMA channel names",
                                       default="headers.txt")
 
@@ -212,7 +219,7 @@ class IMPORT_OT_image_to_plane(bpy.types.Operator, ImportHelper, AddObjectHelper
         editmode = context.user_preferences.edit.use_enter_edit_mode
         context.user_preferences.edit.use_enter_edit_mode = False
         if context.active_object and context.active_object.mode == 'EDIT':
-            bpy.ops.object.mode_set(mode='OBJECT')
+            ops.object.mode_set(mode='OBJECT')
 
         import_sweep(self, context)
 
@@ -225,12 +232,12 @@ def import_ema_sweep_button(self, context):
     self.layout.operator(IMPORT_OT_image_to_plane.bl_idname, text="AG500 EMA Sweep", icon='PLUGIN')
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_import.append(import_ema_sweep_button)
+    utils.register_module(__name__)
+    types.INFO_MT_file_import.append(import_ema_sweep_button)
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_import.remove(import_ema_sweep_button)
+    utils.unregister_module(__name__)
+    types.INFO_MT_file_import.remove(import_ema_sweep_button)
 
 if __name__ == '__main__':
     register()
