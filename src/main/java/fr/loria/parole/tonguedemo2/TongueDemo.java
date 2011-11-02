76f9358a8aedef173ebb9a3e66c39d7ad9112d6a
Ingmar Steiner (burth.loria.fr) <ingmar.steiner@inria.fr>
2011-11-02 15:11:21 +0100

    barebones lwjgl ardor3d app using maven-nativedependencies-plugin

--- src/main/java/fr/loria/parole/tonguedemo2/TongueDemo.java
+++ src/main/java/fr/loria/parole/tonguedemo2/TongueDemo.java
@@ -1,12 +1,96 @@
 package fr.loria.parole.tonguedemo2;
 
-import javax.swing.JFrame;
+import com.ardor3d.framework.DisplaySettings;
+import com.ardor3d.framework.Scene;
+import com.ardor3d.framework.lwjgl.LwjglCanvas;
+import com.ardor3d.framework.lwjgl.LwjglCanvasRenderer;
+import com.ardor3d.intersection.PickResults;
+import com.ardor3d.math.Ray3;
+import com.ardor3d.renderer.Renderer;
+import com.ardor3d.scenegraph.Node;
+import com.ardor3d.util.ContextGarbageCollector;
+import com.ardor3d.util.Timer;
 
-public class TongueDemo {
+/**
+ * Borrowing heavily from <a href=
+ * "http://ardorlabs.trac.cvsdude.com/Ardor3Dv1/browser/trunk/ardor3d-examples/src/main/java/com/ardor3d/example/basic/LwjglBasicExample.java?rev=1745"
+ * >LwjglBasicExample</a>
+ * 
+ * @author steiner
+ * 
+ */
+public class TongueDemo implements Scene {
+
+	private LwjglCanvas canvas;
+	private Timer timer = new Timer();
+	private final Node root = new Node();
+	private boolean exit;
+
+	TongueDemo() {
+		canvas = initLwjgl();
+		canvas.init();
+	}
+
+	private LwjglCanvas initLwjgl() {
+		final LwjglCanvasRenderer canvasRenderer = new LwjglCanvasRenderer(this);
+		final DisplaySettings settings = new DisplaySettings(800, 600, 24, 0, 0, 8, 0, 0, false, false);
+		return new LwjglCanvas(canvasRenderer, settings);
+	}
+
+	private void start() {
+		initExample();
+
+		// Run in this same thread.
+		while (!exit) {
+			updateExample();
+			canvas.draw(null);
+			Thread.yield();
+		}
+		canvas.getCanvasRenderer().makeCurrentContext();
+
+		// Done, do cleanup
+		ContextGarbageCollector.doFinalCleanup(canvas.getCanvasRenderer().getRenderer());
+		canvas.close();
+
+		canvas.getCanvasRenderer().releaseCurrentContext();
+	}
+
+	private void initExample() {
+		// TODO Auto-generated method stub
+	}
+
+	private void updateExample() {
+		if (canvas.isClosing()) {
+			exit = true;
+			return;
+		}
+
+		timer.update();
+
+		// Update controllers/render states/transforms/bounds for rootNode.
+		root.updateGeometricState(timer.getTimePerFrame(), true);
+	}
 
 	public static void main(String args[]) {
-		JFrame frame = new JFrame();
-		frame.setSize(800, 600);
-		frame.setVisible(true);
+		TongueDemo demo = new TongueDemo();
+		demo.start();
+	}
+
+	@Override
+	public boolean renderUnto(Renderer renderer) {
+		if (!canvas.isClosing()) {
+
+			// Draw the root and all its children.
+			renderer.draw(root);
+
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public PickResults doPick(Ray3 pickRay) {
+        // Ignore
+        return null;
 	}
 }
