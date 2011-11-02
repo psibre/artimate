64524c1b1be545e1af1b4f3e4e9b06c8996b9a54
Ingmar Steiner (burth.loria.fr) <ingmar.steiner@inria.fr>
2011-11-02 12:32:14 +0100

    barebones swing app

---
+++ src/main/java/fr/loria/parole/tonguedemo2/TongueDemo.java
@@ -0,0 +1,12 @@
+package fr.loria.parole.tonguedemo2;
+
+import javax.swing.JFrame;
+
+public class TongueDemo {
+
+	public static void main(String args[]) {
+		JFrame frame = new JFrame();
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+	}
+}
