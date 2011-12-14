96d2d28e936df0b3542778e8ac768782592fb1a7
Ingmar Steiner (burth.loria.fr) <ingmar.steiner@inria.fr>
2011-12-14 17:00:22 +0100

    raw i/o optimization for pos concatenation

--- EMATK/CatPos.py
+++ EMATK/CatPos.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
-import argparse, ema, glob, os
+import argparse, glob, os
+#import ema
+
+chunksize = 8192
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input-directory", dest="indir",
                     help="Input directory containing .pos files in a pos subdirectory")
 parser.add_argument("-o", "--output-directory", dest="outdir",
                     help="Output directory")
+parser.add_argument("-c", "--chunksize", dest="chunksize", type=int, default=chunksize,
+                    help="Chunk size for binary I/O (default: %d)" % chunksize)
 args = parser.parse_args()
 
 posfiles = glob.glob("%s/pos/*.pos" % args.indir)
@@ -22,7 +27,15 @@ outfile = open(outfilename, 'wb')
 print("Opened %s for writing" % outfilename) 
 
 for posfile in posfiles:
-    ema.Sweep(posfile).save(outfile)
+    # OO handling:
+    #ema.Sweep(posfile).save(outfile)
+    # but we just want raw speed here:
+    with open(posfile, 'rb') as infile:
+        while True:
+            chunk = infile.read(args.chunksize)
+            if not chunk:
+                break
+            outfile.write(chunk)
     print("Appended %s" % posfile)
 
 outfile.close()
