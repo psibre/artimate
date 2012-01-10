9f410b2cecccacdb03c00c50d7e5ebd323599d33
Ingmar Steiner (burth.loria.fr) <ingmar.steiner@inria.fr>
2012-01-10 10:51:24 +0100

    redesigned units with an extensible feature map

---
+++ TongueDemo2/src/main/java/fr/loria/parole/artimate/data/Features.java
@@ -0,0 +1,5 @@
+package fr.loria.parole.artimate.data;
+
+public enum Features {
+	DURATION, LABEL
+}
--- TongueDemo2/src/main/java/fr/loria/parole/artimate/data/Unit.java
+++ TongueDemo2/src/main/java/fr/loria/parole/artimate/data/Unit.java
@@ -1,23 +1,37 @@
 package fr.loria.parole.artimate.data;
 
+import java.util.EnumMap;
+
 public class Unit {
-	protected double duration;
-	protected String label;
+	protected EnumMap<Features, Object> features = new EnumMap<Features, Object>(Features.class);
+
+	public Unit() {
+	}
 
 	public Unit(double duration) {
 		this(duration, "");
 	}
 
 	public Unit(double duration, String label) {
-		this.duration = duration;
-		this.label = label;
+		setDuration(duration);
+		setLabel(label);
 	}
 
 	public double getDuration() {
+		double duration = (Double) features.get(Features.DURATION);
 		return duration;
 	}
 
 	public String getLabel() {
+		String label = (String) features.get(Features.LABEL);
 		return label;
+	}
+
+	protected void setDuration(double duration) {
+		features.put(Features.DURATION, duration);
+	}
+
+	protected void setLabel(String label) {
+		features.put(Features.LABEL, label);
 	}
 }
